fceb2224e2cf8d10f35980ae9150bd74ae5cd057
Karel Haapasalo <karel.haapasalo@hotmail.com>
2021-04-12 12:46:12 +0300

    created static Shopping Cart

---
+++ src/components/ShoppingCart.js
@@ -0,0 +1,14 @@
+function ShoppingCart() {
+    return <div className="productsContainer">
+                <h2>SHOPPING CART</h2>
+
+                <div className="checkOutButton">
+                    <h3>Checkout</h3>
+                </div>
+                <div className="totalAmount">
+                    <p>19,90€</p>
+                </div>
+            </div>
+}
+
+export default ShoppingCart;
